30b69474891a70574d10ca5a823d87599b56bda3
Mayank-23 <mayank.nigam23@gmail.com>
2023-06-22 03:17:18 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input placeholder='name' />
+      
+      <input placeholder ='password'/>
+      <button placeholder='Click'/>
+      <></>
+
+    </div>
+  );
+}
+
+export default App;
